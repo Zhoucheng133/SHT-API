35e7b253115fcee260f75eda05216e3e9324107d
zhoucheng <zhoucheng133@outlook.com>
2025-08-17 19:12:23 +0800

    添加某一天最高温和最低温

--- app.py
+++ app.py
@@ -28,3 +28,25 @@ def getMaxTemp():
 @app.get("/get/min")
 def getMinTemp():
     return cht.getMinTemp()
+
+@app.get("/get/maxByDay")
+def getMaxByDat(year: int, month: int, day: int):
+    try:
+        target_day = datetime(year, month, day)
+    except ValueError:
+        return {
+            "ok": False,
+            "msg": "无效日期"
+        }
+    return cht.getMaxByDay(target_day)
+
+@app.get("/get/minByDay")
+def getMinByDat(year: int, month: int, day: int):
+    try:
+        target_day = datetime(year, month, day)
+    except ValueError:
+        return {
+            "ok": False,
+            "msg": "无效日期"
+        }
+    return cht.getMinByDay(target_day)
